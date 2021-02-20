c9a61119a5c0c1a881977b68b7b382a1103f8522
Nikhil Yogendra Murali <nnky@amazon.com>
2021-02-19 21:36:14 -0800

    fix: show all custom skills in download option

--- src/askContainer/commands/listSkills.ts
+++ src/askContainer/commands/listSkills.ts
@@ -5,12 +5,38 @@ import * as model from 'ask-smapi-model';
 import { EN_US_LOCALE, DEFAULT_PROFILE } from '../../constants';
 
 import skillSummary = model.v1.skill.SkillSummary;
+import skillManagementServiceClient = model.services.skillManagement.SkillManagementServiceClient;
 import { Logger } from '../../logger';
 import { loggableAskError } from '../../exceptions';
 
 export class ListSkillsCommand extends AbstractCommand<Array<SmapiResource<skillSummary>>> {
     constructor() {
         super('ask.container.listSkills');
+    }
+
+    private async getSkillsList(
+        smapiClientInstance: skillManagementServiceClient, vendorId: string, nextToken: undefined | string, 
+        skillsList: Array<SmapiResource<skillSummary>>): Promise<Array<SmapiResource<skillSummary>>> {
+        const listSkills: model.v1.skill.ListSkillResponse = await smapiClientInstance.listSkillsForVendorV1(
+            vendorId, nextToken);
+        listSkills.skills?.forEach((listSkill) => {
+            let skillName = 'someSkill';
+            if (listSkill.nameByLocale !== undefined && Object.values(listSkill.nameByLocale).length > 0) {
+                if (listSkill.nameByLocale[EN_US_LOCALE]) {
+                    skillName = listSkill.nameByLocale[EN_US_LOCALE];
+                } else {
+                    skillName = Object.values(listSkill.nameByLocale)[0];
+                }
+            }
+            skillsList.push(new SmapiResource<skillSummary>(listSkill, skillName));
+        });
+
+        if (listSkills.isTruncated ?? true) {
+            await this.getSkillsList(smapiClientInstance, vendorId, listSkills.nextToken, skillsList);
+        }
+
+
+        return skillsList;
     }
 
     async execute(context: CommandContext): Promise<Array<SmapiResource<skillSummary>>> {
@@ -25,21 +51,8 @@ export class ListSkillsCommand extends AbstractCommand<Array<SmapiResource<skill
             throw loggableAskError(`Failed to retrieve vendorID for profile ${profile}`, err, true);
         }
 
-        
-
-        const listSkills: model.v1.skill.ListSkillResponse = await SmapiClientFactory.getInstance(profile, context.extensionContext).listSkillsForVendorV1(
-            vendorId);
-        listSkills.skills?.forEach((listSkill) => {
-            let skillName = 'someSkill';
-            if (listSkill.nameByLocale !== undefined && Object.values(listSkill.nameByLocale).length > 0) {
-                if (listSkill.nameByLocale[EN_US_LOCALE]) {
-                    skillName = listSkill.nameByLocale[EN_US_LOCALE];
-                } else {
-                    skillName = Object.values(listSkill.nameByLocale)[0];
-                }
-            }
-            skills.push(new SmapiResource<skillSummary>(listSkill, skillName));
-        });
+        await this.getSkillsList(
+            SmapiClientFactory.getInstance(profile, context.extensionContext), vendorId, undefined, skills);
         return skills;
     }
 }
