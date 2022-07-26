7396d89ddc728bfe4d8811759d7511aeb150f8bf
Tyler Donelson <stydonel@amazon.com>
2022-07-25 19:59:13 -0500

    chore: formatting profileManagerWebview.test.ts

--- test/askContainer/webview/profileManagerWebview.test.ts
+++ test/askContainer/webview/profileManagerWebview.test.ts
@@ -6,9 +6,9 @@
 import * as assert from "assert";
 import * as sinon from "sinon";
 import * as vscode from "vscode";
-import { ProfileManagerWebview } from "../../../src/askContainer/webViews/profileManagerWebview";
+import {ProfileManagerWebview} from "../../../src/askContainer/webViews/profileManagerWebview";
 import {FakeExtensionContext, FakeWebviewPanelOnDidChangeViewStateEvent} from "../../testUtilities";
-import { EXTENSION_STATE_KEY } from "../../../src/constants";
+import {EXTENSION_STATE_KEY} from "../../../src/constants";
 
 describe("Webview_deploySkill tests", () => {
   let webView: ProfileManagerWebview;
@@ -27,7 +27,7 @@ describe("Webview_deploySkill tests", () => {
     fakeContext.globalState.update(EXTENSION_STATE_KEY.LWA_PROFILE, "fakeProfileName");
     webView = new ProfileManagerWebview("fakeTitle", "fakeID", fakeContext);
 
-    const message = {deleteProfile : "fakeProfileName"};
+    const message = {deleteProfile: "fakeProfileName"};
     const spy = sinon.spy(vscode.window, "showErrorMessage");
 
     await webView.onReceiveMessageListener(message);
